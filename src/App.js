cc24d5a2484fd62bf263d91d15d5c2955029188c
Igo Gomes <igogomes0@gmail.com>
2023-04-27 01:33:56 -0300

    [igogomes] First commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>My Little Blog</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
