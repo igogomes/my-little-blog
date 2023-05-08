fe848820b65aa84db5cb661e8093798cd1140f05
Igo Gomes <igogomes0@gmail.com>
2023-05-08 02:21:25 -0300

    [igogomes] Including db server in project and rendering contact options dinamically in contact form

--- src/components/layout/Form/ContactForm.js
+++ src/components/layout/Form/ContactForm.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import styles from './ContactForm.module.css'
 import Input from './Input'
 import Select from './Select'
@@ -5,11 +6,27 @@ import SubmitButton from './SubmitButton'
 import TextArea from './TextArea';
 
 function Form({ btnTitle, direction }) {
+    const [subjects, setSubjects] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/subjects", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setSubjects(data)
+        })
+        .catch(err => console.log(err));
+    }, []);
+
     return (
         <form className={styles.form}>
             <Input type="text" text="Digite seu nome" name="name" placeholder="Digite seu nome"/>
             <Input type="email" text="Digite seu e-mail" name="email" placeholder="Digite seu email"/>
-            <Select name="subject" text="Selecione um assunto"/> 
+            <Select name="subject" text="Selecione um assunto" options={subjects}/> 
             <TextArea name="message" id="message" rows="5" placeholder="Digite sua mensagem aqui..."/>
             <SubmitButton text={btnTitle} to={direction} />
         </form>
